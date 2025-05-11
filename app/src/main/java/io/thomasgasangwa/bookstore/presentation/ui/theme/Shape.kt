e007bb1df3c4a7978fd9a3a48f102e63c1fb9d9a
gasangw <thomasgasangwa@gmail.com>
2025-05-11 20:34:27 +0200

    refactor: create presentation, domain and data packages

--- app/src/main/java/io/thomasgasangwa/bookstore/presentation/ui/theme/Shape.kt
+++ app/src/main/java/io/thomasgasangwa/bookstore/presentation/ui/theme/Shape.kt
@@ -1,4 +1,4 @@
-package io.thomasgasangwa.bookstore.ui.theme
+package io.thomasgasangwa.bookstore.presentation.ui.theme
 
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Shapes
--- app/src/main/java/io/thomasgasangwa/bookstore/presentation/ui/theme/Type.kt
+++ app/src/main/java/io/thomasgasangwa/bookstore/presentation/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package io.thomasgasangwa.bookstore.ui.theme
+package io.thomasgasangwa.bookstore.presentation.ui.theme
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
