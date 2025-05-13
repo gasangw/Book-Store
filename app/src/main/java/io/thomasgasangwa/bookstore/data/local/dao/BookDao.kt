6c5b79e3e82ab8d13476713313e9fc06838be779
gasangw <thomasgasangwa@gmail.com>
2025-05-13 10:54:18 +0200

    refactor: add data access methods

--- app/src/main/java/io/thomasgasangwa/bookstore/data/local/dao/BookDao.kt
+++ app/src/main/java/io/thomasgasangwa/bookstore/data/local/dao/BookDao.kt
@@ -1,4 +1,29 @@
 package io.thomasgasangwa.bookstore.data.local.dao
 
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import io.thomasgasangwa.bookstore.data.local.entity.Book
+
+@Dao
 interface BookDao {
+
+   @Insert(onConflict = OnConflictStrategy.IGNORE)
+   suspend fun insert(book: Book)
+
+   @Delete
+   suspend fun delete(book: Book)
+
+   @Update
+   suspend fun update(book: Book)
+
+   @Query("SELECT * FROM books")
+   suspend fun getAllBooks(): List<Book>
+
+   @Query("SELECT * FROM books WHERE id = :id")
+   suspend fun getBookById(id: Int): Book?
+
 }
--- app/src/main/java/io/thomasgasangwa/bookstore/data/local/entity/BookEntity.kt
+++ app/src/main/java/io/thomasgasangwa/bookstore/data/local/entity/BookEntity.kt
@@ -4,7 +4,7 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "books")
-data class BookEntity(
+data class Book(
    @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val title: String,
     val author: String,
