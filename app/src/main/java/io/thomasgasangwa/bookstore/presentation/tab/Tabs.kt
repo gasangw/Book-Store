c00e41f066af197ea1c27a8408fbc26f73b54584
gasangw <thomasgasangwa@gmail.com>
2025-05-23 20:46:42 +0200

    refactor: modify the tabs sections

--- app/src/main/java/io/thomasgasangwa/bookstore/presentation/tab/Tabs.kt
+++ app/src/main/java/io/thomasgasangwa/bookstore/presentation/tab/Tabs.kt
@@ -80,7 +80,7 @@ private fun TabsPreview() {
             titles = titles,
             onTabClicked = { index, -> },
             onAddBookButtonClicked = {},
-            onBookClicked = { _, -> },
+            onBookClicked = {_, ->},
             modifier = Modifier
         )
     }
