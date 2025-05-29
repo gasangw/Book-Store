e27be2a4fa0ea955380ec345b79d15f546a8f783
gasangw <thomasgasangwa@gmail.com>
2025-05-29 11:40:48 +0200

    test: write tests for app bar

---
+++ app/src/androidTest/java/io/thomasgasangwa/bookstore/AppBarTest.kt
@@ -0,0 +1,86 @@
+package io.thomasgasangwa.bookstore
+
+import androidx.compose.ui.test.hasAnyDescendant
+import androidx.compose.ui.test.hasContentDescription
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import io.thomasgasangwa.bookstore.presentation.navigation.AppBar
+import io.thomasgasangwa.bookstore.presentation.navigation.AppNavigationScreens
+import org.junit.Rule
+import org.junit.Test
+
+class AppBarTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun bookStore_appBar() {
+        val currentScreen = AppNavigationScreens.Tabs
+        composeTestRule.setContent {
+            AppBar(
+                currentScreen = currentScreen,
+                canNavigateBack = false,
+                navigateUp = {}
+            )
+        }
+        composeTestRule.onNodeWithText("Book Store")
+            .assertExists()
+    }
+
+    @Test
+    fun addBook_appBar() {
+        val currentScreen = AppNavigationScreens.AddBook
+        composeTestRule.setContent {
+            AppBar(
+                currentScreen = currentScreen,
+                canNavigateBack = true,
+                navigateUp = {}
+            )
+        }
+        composeTestRule.onNodeWithText("Add Book")
+            .assertExists()
+        composeTestRule.onNode(
+            hasAnyDescendant(
+                hasContentDescription("Back arrow")
+            )
+        )
+    }
+
+    @Test
+    fun detailsBook_appBar() {
+        val currentScreen = AppNavigationScreens.BookDetails
+        composeTestRule.setContent {
+            AppBar(
+                currentScreen = currentScreen,
+                canNavigateBack = true,
+                navigateUp = {}
+            )
+        }
+        composeTestRule.onNodeWithText("Details")
+            .assertExists()
+        composeTestRule.onNode(
+            hasAnyDescendant(
+                hasContentDescription("Back arrow")
+            )
+        )
+    }
+
+    @Test
+    fun editBook_appBar() {
+        val currentScreen = AppNavigationScreens.EditBook
+        composeTestRule.setContent {
+            AppBar(
+                currentScreen = currentScreen,
+                canNavigateBack = true,
+                navigateUp = {}
+            )
+        }
+        composeTestRule.onNodeWithText("Edit")
+            .assertExists()
+        composeTestRule.onNode(
+            hasAnyDescendant(
+                hasContentDescription("Back arrow")
+            )
+        )
+    }
+}
