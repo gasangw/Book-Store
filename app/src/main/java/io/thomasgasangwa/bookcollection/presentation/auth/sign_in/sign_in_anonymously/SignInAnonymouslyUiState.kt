d8a0f54ef6871601ba69556f39b235dc6f8843b4
gasangw <thomasgasangwa@gmail.com>
2025-07-04 09:30:37 +0200

    feat:create the signInAnonymous viewModel

--- app/src/main/java/io/thomasgasangwa/bookcollection/presentation/auth/sign_in/sign_in_anonymously/SignInAnonymouslyUiState.kt
+++ app/src/main/java/io/thomasgasangwa/bookcollection/presentation/auth/sign_in/sign_in_anonymously/SignInAnonymouslyUiState.kt
@@ -1,5 +1,7 @@
 package io.thomasgasangwa.bookcollection.presentation.auth.sign_in.sign_in_anonymously
 
 data class SignInAnonymouslyUiState(
-    val isLoading: Boolean = false
+    val isLoading: Boolean = false,
+    val isSignedInSuccessfully: Boolean = false,
+    val errorMessage: String
 )
---
+++ app/src/main/java/io/thomasgasangwa/bookcollection/presentation/auth/sign_in/sign_in_anonymously/SignInAnonymouslyViewModel.kt
@@ -0,0 +1,7 @@
+package io.thomasgasangwa.bookcollection.presentation.auth.sign_in.sign_in_anonymously
+
+import androidx.lifecycle.ViewModel
+
+class SignInAnonymouslyViewModel : ViewModel() {
+
+}
