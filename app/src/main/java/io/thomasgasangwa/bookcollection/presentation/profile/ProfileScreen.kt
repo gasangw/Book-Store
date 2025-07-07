e74a0f076edcb2140e79906657a7428706ad8bf6
gasangw <thomasgasangwa@gmail.com>
2025-07-07 11:02:06 +0200

    feat: create the profile page

---
+++ app/src/main/java/io/thomasgasangwa/bookcollection/presentation/profile/ProfileScreen.kt
@@ -0,0 +1,67 @@
+package io.thomasgasangwa.bookcollection.presentation.profile
+
+import BookStoreTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import io.thomasgasangwa.bookcollection.R
+
+@Composable
+fun ProfileScreen(modifier: Modifier = Modifier) {
+    val user = ""
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(top = 18.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Box(
+            modifier = modifier
+                .size(150.dp)
+                .clip(CircleShape)
+                .background(color = MaterialTheme.colorScheme.outlineVariant)
+        ) {
+            Image(
+                painter = painterResource(R.drawable.outline_person_24),
+                contentDescription = "profile",
+                contentScale = ContentScale.Crop,
+                modifier = modifier
+                    .matchParentSize()
+                    .padding(10.dp)
+                    .clip(CircleShape)
+            )
+        }
+
+        Text(
+            text = if (user.isEmpty()) "Anonymous User" else "Thomas Gasangwa",
+            style = MaterialTheme.typography.titleLarge,
+        )
+        Text(
+            text = if (user.isEmpty()) "example@gmail.com" else "thomasgasangwa@gmail.com",
+            style = MaterialTheme.typography.bodyMedium,
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun ProfileScreenPreview() {
+    BookStoreTheme {
+        ProfileScreen()
+    }
+}
