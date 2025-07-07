40dc798f83d0facfb23bb402f5b742fb13839903
gasangw <thomasgasangwa@gmail.com>
2025-07-07 15:51:05 +0200

    refactor: disable past days and enable users to only see current date and future date

--- app/src/main/java/io/thomasgasangwa/bookcollection/presentation/borrow_dialog/BorrowDialog.kt
+++ app/src/main/java/io/thomasgasangwa/bookcollection/presentation/borrow_dialog/BorrowDialog.kt
@@ -1,12 +1,14 @@
 package io.thomasgasangwa.bookcollection.presentation.borrow_dialog
 
 import BookStoreTheme
+import android.icu.util.Calendar
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.BasicAlertDialog
 import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.DateRangePicker
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.SelectableDates
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.rememberDateRangePickerState
@@ -14,6 +16,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import timber.log.Timber
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -41,9 +44,23 @@ fun DateRangePickerModal(
     onDateRangeSelected: (Pair<Long?, Long?>) -> Unit,
     onDismiss: () -> Unit
 ) {
-    val dateRangePickerState = rememberDateRangePickerState()
+    val dateRangePickerState = rememberDateRangePickerState(
+        selectableDates = object : SelectableDates {
+            override fun isSelectableDate(utcTimeMillis: Long): Boolean {
+                val today = Calendar.getInstance().apply {
+                    set(Calendar.HOUR_OF_DAY, 0)
+                    set(Calendar.MINUTE, 0)
+                    set(Calendar.SECOND, 0)
+                    set(Calendar.MILLISECOND, 0)
+                }.timeInMillis
+                return utcTimeMillis >= today
 
-//    Timber.d("DateRangePickerModal $dateRangePickerState")
+            }
+        }
+    )
+
+    Timber.e("here is the start date ${dateRangePickerState.selectedStartDateMillis}")
+    Timber.e("here is the end date ${dateRangePickerState.selectedEndDateMillis}")
 
     DatePickerDialog(
         onDismissRequest = onDismiss,
