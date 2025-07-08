36e1d7990025acc77af99ba5ff0af447cb6496c1
gasangw <thomasgasangwa@gmail.com>
2025-07-08 21:03:58 +0200

    refactor: add typeconvert annotation for database to understand

--- app/src/main/java/io/thomasgasangwa/bookcollection/data/local/database/BookDatabase.kt
+++ app/src/main/java/io/thomasgasangwa/bookcollection/data/local/database/BookDatabase.kt
@@ -4,13 +4,16 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import io.thomasgasangwa.bookcollection.data.local.dao.BookDao
 import io.thomasgasangwa.bookcollection.data.local.dao.BookingDao
 import io.thomasgasangwa.bookcollection.data.local.entity.BookEntity
 import io.thomasgasangwa.bookcollection.data.local.entity.BookingEntity
+import io.thomasgasangwa.bookcollection.data.local.mapper.Converters
 
 
 @Database(entities = [BookEntity::class, BookingEntity::class], version = 1, exportSchema = false)
+@TypeConverters(Converters::class)
 abstract class BookDatabase : RoomDatabase() {
     abstract fun bookDao(): BookDao
     abstract fun bookingDao(): BookingDao
--- app/src/main/java/io/thomasgasangwa/bookcollection/data/local/mapper/Converters.kt
+++ app/src/main/java/io/thomasgasangwa/bookcollection/data/local/mapper/Converters.kt
@@ -1,12 +1,15 @@
 package io.thomasgasangwa.bookcollection.data.local.mapper
 
+import androidx.room.TypeConverter
 import io.thomasgasangwa.bookcollection.presentation.bookings.BookingStatus
 
 class Converters {
+    @TypeConverter
     fun fromBookingStatus(status: BookingStatus?): String? {
         return status?.name
     }
 
+    @TypeConverter
     fun toBookingStatus(status: String?): BookingStatus? {
         return status?.let { BookingStatus.valueOf(it) }
     }
