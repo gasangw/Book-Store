094f1a40671af2b94e6bd74122f2a3d9169f69d6
gasangw <thomasgasangwa@gmail.com>
2025-07-08 11:55:28 +0200

    refactor: implement the userStateHolderImplementation

--- app/src/main/java/io/thomasgasangwa/bookcollection/common/state/UserStateHolderImpl.kt
+++ app/src/main/java/io/thomasgasangwa/bookcollection/common/state/UserStateHolderImpl.kt
@@ -1,4 +1,26 @@
 package io.thomasgasangwa.bookcollection.common.state
 
-class UserStateHolderImpl {
+import io.thomasgasangwa.bookcollection.domain.model.User
+import io.thomasgasangwa.bookcollection.globalstate.UserStateHolder
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class UserStateHolderImpl : UserStateHolder {
+    private val _userState = MutableStateFlow(UserState())
+    override val userState: StateFlow<UserState> = _userState.asStateFlow()
+
+    override fun updateUser(user: User) {
+        _userState.update { current ->
+            current.copy(
+                isLoggedIn = true,
+                user = user
+            )
+        }
+    }
+
+    override fun clearUser() {
+        _userState.update { UserState() }
+    }
 }
